fe555a178fd11db5c05d431ce2301b6bc17b88fd
Danting Huang <serena.huangdanting@gmail.com>
2022-03-21 23:28:49 -0400

    Complete board through col conflicts

--- src/Board.js
+++ src/Board.js
@@ -79,27 +79,53 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var row = this.get(rowIndex);
+      var count = 0;
+      for (var i = 0; i < row.length; i++) {
+        if (row[i] === 1) {
+          count++;
+        }
+      }
+      return count > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var boardMatrix = this.rows();
+      for (var i = 0; i < boardMatrix.length; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
-
-
 
     // COLUMNS - run from top to bottom
     // --------------------------------------------------------------
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var boardMatrix = this.rows();
+      var rowCount = this.get('n');
+      var count = 0;
+      for (var i = 0; i < rowCount; i++) {
+        if (boardMatrix[i][colIndex] === 1) {
+          count++;
+        }
+      }
+      return count > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var boardMatrix = this.rows();
+      var colCount = this.get('n');
+      for (var i = 0; i < colCount; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
