4e5e8b429eabbd9ea8fb639b8cf1a8feb70c58fd
Danting Huang <serena.huangdanting@gmail.com>
2022-03-26 14:41:27 -0400

    optimized Time complexity for countNQueensSolution and passing all the test.

--- src/solvers.js
+++ src/solvers.js
@@ -104,29 +104,70 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solution = new Board({'n': n});
-
-  let solutionCount = 0;
-  var backtrack = function(rowIndex) {
-    if (rowIndex === n) {
-
-      solutionCount++;
-      return;
-    }
-    for (var colIndex = 0; colIndex < n; colIndex++) {
-      //debugger;
-      if (solution.isValidQueenPosition(rowIndex, colIndex)) {
-        solution.togglePiece(rowIndex, colIndex);
-        backtrack(rowIndex + 1);
-        solution.togglePiece(rowIndex, colIndex);
-
+  var solutionCount = 0;
+  if (n === 0) {
+    solutionCount = 1;
+  } else if (n === 2 || n === 3) {
+    solutionCount = 0;
+  } else {
+    var queensInColumn = new Array(n).fill(false);
+    var queensInMajorDiagonal = new Array(n * 2 - 1).fill(false);
+    var queensInMinorDiagonal = new Array(n * 2 - 1).fill(false);
+    var helperFunction = function(rowIndex, queensInColumn, queensInMajorDiagonal, queensInMinorDiagonal) {
+      if (rowIndex === n) {
+        return 1;
       }
-    }
+      var count = 0;
+      for (var colIndex = 0; colIndex < n; colIndex++) {
+        if (colIndex - rowIndex >= 0) {
+          var majorDiagonalIndex = colIndex - rowIndex;
+        } else {
+          var majorDiagonalIndex = (colIndex - rowIndex) + (n * 2 - 1);
+        }
+        var minorDiagonalIndex = rowIndex + colIndex;
+        if (!queensInColumn[colIndex] && !queensInMajorDiagonal[majorDiagonalIndex] && !queensInMinorDiagonal[minorDiagonalIndex]) {
+          queensInColumn[colIndex] = true;
+          queensInMajorDiagonal[majorDiagonalIndex] = true;
+          queensInMinorDiagonal[minorDiagonalIndex] = true;
+          count += helperFunction(rowIndex + 1, queensInColumn, queensInMajorDiagonal, queensInMinorDiagonal);
+          queensInColumn[colIndex] = false;
+          queensInMajorDiagonal[majorDiagonalIndex] = false;
+          queensInMinorDiagonal[minorDiagonalIndex] = false;
+        }
+      }
+      return count;
+    };
+    solutionCount = helperFunction(0, queensInColumn, queensInMajorDiagonal, queensInMinorDiagonal);
+  }
 
-  };
-  backtrack(0);
+
+
+
 
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+// Original Code --> countNQueensSolutions
+// var solution = new Board({'n': n});
+
+// let solutionCount = 0;
+// var backtrack = function(rowIndex) {
+//   if (rowIndex === n) {
+
+//     solutionCount++;
+//     return;
+//   }
+//   for (var colIndex = 0; colIndex < n; colIndex++) {
+//     //debugger;
+//     if (solution.isValidQueenPosition(rowIndex, colIndex)) {
+//       solution.togglePiece(rowIndex, colIndex);
+//       backtrack(rowIndex + 1);
+//       solution.togglePiece(rowIndex, colIndex);
+
+//     }
+//   }
+
+// };
+// backtrack(0);
